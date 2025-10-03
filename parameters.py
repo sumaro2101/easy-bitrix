940dac90794966606329a7ba0da870239bafe848
Alex Pavlov <yukiu217@gmail.com>
2025-10-03 23:50:18 +0300

    add parameters

--- parameters.py
+++ parameters.py
@@ -1,14 +1,44 @@
+from common import ProductFields
+
 
 class Parameter:
     """
     """
     @staticmethod
     def ID(value: int) -> str:
-        return '"ID":{}'.format(str(value))
+        return f'{ProductFields.ID.value}:{str(value)}'
 
     @staticmethod
     def TITLE(value: str) -> str:
-        return '"TITLE":{}'.format(str(value))
+        return f'{ProductFields.TITLE.value}:"{str(value)}"'
+
+    @staticmethod
+    def TYPE_ID(value: int) -> str:
+        return f'{ProductFields.TYPE_ID.value}:{str(value)}'
+
+    @staticmethod
+    def CATEGORY_ID(value: int) -> str:
+        return f'{ProductFields.CATEGORY_ID.value}:{str(value)}'
+
+    @staticmethod
+    def STAGE_ID(value: str) -> str:
+        return f'{ProductFields.STAGE_ID.value}:"{str(value)}"'
+
+    @staticmethod
+    def OPPORTUNITY(value: float) -> str:
+        return f'{ProductFields.OPPORTUNITY.value}:{str(value)}'
+
+    @staticmethod
+    def IS_MANUAL_OPPORTUNITY(value: bool) -> str:
+        return f'{ProductFields.IS_MANUAL_OPPORTUNITY.value}:{str(int(value))}'
+
+    @staticmethod
+    def ASSIGNED_BY_ID(value: int) -> str:
+        return f'{ProductFields.ASSIGNED_BY_ID.value}:{str(value)}'
+
+    @staticmethod
+    def DATE_CREATE(value: str) -> str:
+        return f'{ProductFields.DATE_CREATE.value}:"{str(value)}"'
 
 
 class BussParameters:
